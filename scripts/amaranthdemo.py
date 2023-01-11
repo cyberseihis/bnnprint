d31b5983f52dd17b479a81aab7263d609f7b0e17
cyberseihis <mcraftdm@gmail.com>
2023-01-11 21:38:02 +0200

    changeofplans

---
+++ scripts/amaranthdemo.py
@@ -0,0 +1,12 @@
+# import amaranth
+
+# hardcode weights
+
+# first layer weights to hdl
+
+# numpy caclulation (copy paste from pendig1 scipt)
+
+# test equality
+
+# turn to verilog
+# test with iverilog
