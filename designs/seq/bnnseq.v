e76fad2329e16aec282b73f8d61cceada41de9f8
cyberseihis <mcraftdm@gmail.com>
2023-03-05 22:14:18 +0200

    First attempt at full bnn test. Hopefully just upside down

---
+++ designs/seq/bnnseq.v
@@ -0,0 +1,28 @@
+module bnnseq #(
+  parameter N = 4,
+  parameter B = 4,
+  parameter M = 4,
+  parameter C = 4
+  ) (
+  input clk,
+  input rst,
+  input [N*B-1:0] data,
+  output [$clog2(C)-1:0] klass
+  );
+  
+  localparam SumL = $clog2(M+1);
+  wire [SumL*C-1:0] sums;
+
+  seqlego #(.N(N),.B(B),.M(M),.C(C)) layer1 (
+    .clk(clk),
+    .rst(rst),
+    .data(data),
+    .out(sums)
+  );
+
+  argmax #(.N(C),.I($clog2(C)),.K(SumL)) result (
+     .inx(sums),
+     .outimax(klass)
+  );
+
+endmodule
---
+++ designs/seq/tbbnnseq.v
@@ -0,0 +1,41 @@
+module tbbnnseq;
+
+  localparam N = 16;
+  localparam M = 40;
+  localparam B = 4;
+  localparam C = 10;
+
+  
+  wire [B*N-1:0] data;
+  reg rst;
+  reg clk;
+  
+  assign data =  64'h8f4d96400498fe6f;
+  localparam SumL = $clog2(M+1);
+  wire [$clog2(C)-1:0] klass;
+
+  // Instantiate module under test
+ bnnseq #(.N(N), .B(B), .M(M), .C(C)) dut (
+    .data(data),
+    .clk(clk),
+    .rst(rst),
+    .klass(klass)
+  );
+  
+  always #10 clk <= ~clk;
+
+  // Write output numbers to file
+  initial begin
+    rst <= 1;
+    clk <= 0;
+    #5
+    rst <= 0;
+    #5
+    # 2000
+    $display("huh");
+    $display("klass %d",klass);
+    $finish;
+  end
+
+endmodule
+
