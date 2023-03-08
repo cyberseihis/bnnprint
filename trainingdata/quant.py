4ba700c2764292e97c0ff1cd143912c034ed71d2
cyberseihis <mcraftdm@gmail.com>
2023-03-08 23:00:53 +0200

    Discovery: arrythmia has a constant column

--- trainingdata/quant.py
+++ trainingdata/quant.py
@@ -1,11 +1,13 @@
 import binascii
 import pandas as pd
 import numpy as np
+from dsedit import get_csv_filenames
 
 
 def qnorm(Xa):
     return pd.DataFrame(Xa.apply(
-                        lambda x: (x - np.min(x)) / (np.max(x) - np.min(x))))
+                        lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+                        ).fillna(0)
 
 
 def qfour(Xa):
@@ -22,3 +24,34 @@ def hexdump4(X):
 def hexiby(X):
     Y = [row.to_numpy() for index, row in qfour(X).iterrows()]
     return [hexdump4(x) for x in Y]
+
+
+def write_strings_to_file(strings, file_path):
+    with open(file_path, 'w') as file:
+        for string in strings:
+            file.write(string + '\n')
+
+
+def csv_to_activations(fnm):
+    df = pd.read_csv(fnm, header=None)
+    df = df.iloc[:-1]
+    hd4 = hexiby(df)
+    write_strings_to_file(hd4, fnm+".activations")
+
+
+def mnn():
+    for fnm in get_csv_filenames():
+        csv_to_activations(fnm)
+
+
+def chknonan():
+    for fnm in get_csv_filenames():
+        df = pd.read_csv(fnm, header=None)
+        print(f"{fnm} - {check_constant_columns(df)}")
+
+
+def check_constant_columns(df):
+    for col in df.columns:
+        if df[col].nunique() == 1:
+            return True
+    return False
