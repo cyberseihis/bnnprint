21730c0945d2744d92ef2c66a615b0f4546ea181
cyberseihis <mcraftdm@gmail.com>
2023-03-13 16:16:33 +0200

    Sequencial bnn testbench passes for pendigits

--- designs/seq/tbbnnseq.v
+++ designs/seq/tbbnnseq.v
@@ -15,7 +15,14 @@ module tbbnnseq;
   localparam halfT=period/2;
 
   
-  assign testcases[0] =  64'h8f4d96400498fe6f;
+assign testcases[0] = 64'h8f4d96400498fe6f;
+assign testcases[1] = 64'h0e4f7c572260b0f1;
+assign testcases[2] = 64'h095bceffcc884430;
+assign testcases[3] = 64'h0f1f1b37e5f7c4b0;
+assign testcases[4] = 64'h0b8dffddaa665380;
+
+
+
   localparam SumL = $clog2(M+1);
   wire [$clog2(C)-1:0] klass;
 
@@ -33,7 +40,8 @@ module tbbnnseq;
   initial begin
     /* $monitor("sums %h %0t",dut.sums,$time); */
     /* $monitor("1done %h %0t",dut.layers.layer1.done,$time); */
-    runtestcase(0);
+    for(i=0;i<Ts;i=i+1)
+        runtestcase(i);
     $finish;
   end
 
