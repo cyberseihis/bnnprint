fb20a842ccd2fc501a6bd1013f7d1e7ded6e7444
cyberseihis <mcraftdm@gmail.com>
2023-03-13 18:27:52 +0200

    Added includes/generics to seq bnn testbench

--- designs/seq/tbbnnseq.v
+++ designs/seq/tbbnnseq.v
@@ -1,10 +1,17 @@
-module tbbnnseq;
-
-  localparam N = 16;
-  localparam M = 40;
-  localparam B = 4;
-  localparam C = 10;
-  localparam Ts = 5;
+`ifndef DUTNAME
+`define DUTNAME bnnseq
+`define TBNAME tbbnnseq
+parameter N = 4;
+parameter B = 4;
+parameter M = 4;
+parameter C = 4;
+parameter Ts = 5;
+`endif
+module `TBNAME #(
+`ifdef PARAMS
+`include `PARAMS
+`endif
+)();
 
   
   reg [B*N-1:0] data;
@@ -14,20 +21,15 @@ module tbbnnseq;
   localparam period=10;
   localparam halfT=period/2;
 
+`ifdef TESTCASES
+`include `TESTCASES
+`endif
   
-assign testcases[0] = 64'h8f4d96400498fe6f;
-assign testcases[1] = 64'h0e4f7c572260b0f1;
-assign testcases[2] = 64'h095bceffcc884430;
-assign testcases[3] = 64'h0f1f1b37e5f7c4b0;
-assign testcases[4] = 64'h0b8dffddaa665380;
-
-
-
   localparam SumL = $clog2(M+1);
   wire [$clog2(C)-1:0] klass;
 
   // Instantiate module under test
- blmao #() dut (
+ `DUTNAME #() dut (
     .data(data),
     .clk(clk),
     .rst(rst),
