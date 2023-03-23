d2998de8468a3182f9645b363f533f534ee03530
cyberseihis <mcraftdm@gmail.com>
2023-03-23 20:22:59 +0200

    Put count check insiden binary accumulator for ternary

--- designs/tnnseq/components/teraccum.v
+++ designs/tnnseq/components/teraccum.v
@@ -1,9 +1,10 @@
-module binaccum #(
+module teraccum #(
     parameter N = 4 // Number of elements to add
 )(
     input clk,             // Clock input
     input rst,             // Reset input
-    input put,
+    input ena,
+    input [$clog2(N)-1:0] cnt,
     input unsigned data_in,  // Input data
     output reg unsigned [$clog2(N+1)-1:0] acc
 );
@@ -11,7 +12,7 @@ module binaccum #(
 always @(posedge clk or posedge rst) begin
     if (rst) begin
         acc <= 0;
-    end else if (!put) begin
+    end else if (ena && cnt<N) begin
         acc <= acc+data_in;
     end
 end
