282645021721c2d13b058a4196239e4483fb72ea
cyberseihis <mcraftdm@gmail.com>
2023-03-28 23:47:41 +0300

    Parameterised tbseqtnn, correct class first 5 whitewine

--- designs/tnnseq/components/tblook.v
+++ designs/tnnseq/components/tblook.v
@@ -1,12 +1,16 @@
-module tblook ();
-
-parameter N = 11;
-parameter M = 40;
-parameter B = 4;
-parameter C = 7;
-parameter Ts = 5;
-
-  wire [B*N-1:0] data;
+`timescale 1us/1ns
+`ifndef DUTNAME
+`define DUTNAME tersewine
+`define TBNAME tblook
+`endif
+module `TBNAME #(
+`ifdef PARAMS
+`include `PARAMS
+`else
+`include "piller.par"
+`endif
+)();
+  reg [B*N-1:0] data;
   wire [B*N-1:0] testcases [Ts-1:0];
   reg rst;
   reg clk;
@@ -14,13 +18,21 @@ parameter Ts = 5;
   localparam period=Nsperiod/500;
   localparam halfT=period/2;
 
-assign data = 44'h53352264442;
+assign testcases[0] = 44'h53352264442;
+assign testcases[1] = 44'h43302152854;
+assign testcases[2] = 44'h73422232845;
+assign testcases[3] = 44'h52322373735;
+assign testcases[4] = 44'h52322373735;
 
+`ifdef TESTCASES
+`include `TESTCASES
+`endif
+  
   localparam SumL = $clog2(M+1);
   wire [$clog2(C)-1:0] klass;
 
   // Instantiate module under test
- terseq_wrap #(
+ `DUTNAME #(
  ) dut (
     .data(data),
     .clk(clk),
@@ -34,13 +46,13 @@ assign data = 44'h53352264442;
   initial begin
     /* $monitor("sums %h %0t",dut.sums,$time); */
     /* $monitor("1done %h %0t",dut.layers.layer1.done,$time); */
-    for(i=0;i<1;i=i+1)
+    for(i=0;i<Ts;i=i+1)
         runtestcase(i);
     $finish;
   end
 
   task runtestcase(input integer i); begin
-    /* data <= testcases[i]; */
+    data <= testcases[i];
     rst <= 1;
     clk <= 0;
     #period
@@ -50,7 +62,7 @@ assign data = 44'h53352264442;
     /* thesums(); */
     $display("mid %b",dut.tnn.revmidd);
     $display("sums %h",dut.tnn.out);
-    $display("klass %d",data,(C-1-klass));
+    $display("klass %d %d",data,klass);
     /* $display("%h %d",data,(C-1-klass)); */
   end
   endtask
