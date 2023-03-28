d01329bc95541dbe04fd4be70dd6fadc52289106
cyberseihis <mcraftdm@gmail.com>
2023-03-29 01:15:51 +0300

    Removed obsolete files

--- scripts/larq_context.py
+++
@@ -1,8 +0,0 @@
-import larq as lq
-import keras
-
-
-def contextise(mname: str):
-    model = keras.models.load_model(mname)
-    with lq.context.quantized_scope(True):
-        model.save("bin"+mname)  # save binarized weights
