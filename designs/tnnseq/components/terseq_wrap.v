f337888a799b64ce95e790a987334c694e66f1d5
cyberseihis <mcraftdm@gmail.com>
2023-03-28 22:17:21 +0300

    Wrapper to pass parameters to core seq tnn module

---
+++ designs/tnnseq/components/terseq_wrap.v
@@ -0,0 +1,36 @@
+`ifndef DUTNAME
+`define DUTNAME terseq_wrap
+    `include "killer.wei"
+`else
+    `include `BSTRINGS
+`endif
+module `DUTNAME #(
+`ifdef PARAMS
+`include `PARAMS
+`else
+`include "piller.par"
+`endif
+  ) (
+  input clk,
+  input rst,
+  input [N*B-1:0] data,
+  output [$clog2(C)-1:0] klass
+  );
+
+  seqtenlego #(
+      .N(N),.B(B),.M(M),.C(C),
+  .Wvals(`WVALS),
+  .Wzero(`WZERO),
+  .Wnnz(`WNNZ),
+  .WnnzX(`WNNZX), // Number of not-zeroes of weight matrix
+  .WvalsX(`WVALSX),  // Bits of not-zeroes
+  .WcolX(`WCOLX), // Column of non-zeros
+  .WrowX(`WROWX) // Column of non-zeros // Start indices per row
+      ) tnn (
+    .clk(clk),
+    .rst(rst),
+    .data(data),
+    .klass(klass)
+  );
+
+endmodule
