81c118592e07f75e88aabf146d619a9feb3ed21c
cyberseihis <mcraftdm@gmail.com>
2023-04-23 21:46:06 +0300

    Split sw0 in pos/neg and first/second output val per sample

--- scripts/predict.py
+++ scripts/predict.py
@@ -3,6 +3,14 @@ import pandas as pd
 import numpy as np
 from qkeras import QActivation, QDense
 import keras.backend as K
+from functools import reduce
+from collections import defaultdict
+
+
+def groupBy(key, seq):
+    return reduce(lambda grp, val:
+                  grp[key(val)].append(val)
+                  or grp, seq, defaultdict(list))
 
 
 def get_model(model_f):
@@ -40,7 +48,7 @@ def get_layer_outputs(model, X):
     return output_values
 
 
-def winner_args(model, X, y):
+def winners_only(model, X, y):
     dut = get_results(model, X)
     if len(dut) != len(y):
         raise ValueError("Arrays must be the same length")
@@ -65,7 +73,33 @@ def redo(model, X):
     return [l0, l1, l2, l3]
 
 
+def split_posneg(mat):
+    pos = np.maximum(0, mat)
+    neg = np.minimum(0, mat)
+    return pos, neg
+
+
 def mysign(a):
     signs = np.sign(a)
     signs_no_zero = np.where(signs == 0, 1, signs)
     return signs_no_zero
+
+
+def first_second(row):
+    s = sorted(enumerate(row), key=lambda x: -x[1])[:2]
+    return s
+
+
+def oponents(X):
+    s = [first_second(x) for x in X]
+    s = [((x[0][0], x[1][0]), (x[0][1], x[1][1])) for x in s]
+    sj = groupBy(key=lambda x: x[0], seq=s)
+    lol = {k: [x[1] for x in v] for k, v in sj.items()}
+    return lol
+
+
+def quick():
+    mod = "../models/bnn1/pendigits_bnn1.h5"
+    mod = get_model(mod)
+    X, y = preped_dset("../trainingdata/pendigits.csv")
+    return mod, X, y
