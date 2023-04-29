dd94c88ec617bc120f2a169e77ce567d09b1b925
cyberseihis <mcraftdm@gmail.com>
2023-04-29 18:22:18 +0300

    Function to check which samples error if a bit increases

--- scripts/rounder.py
+++ scripts/rounder.py
@@ -3,13 +3,29 @@ import numpy as np
 
 # To break ties
 def add_to_rows(x, y):
-    mask = np.arange(len(y)) < y[:, np.newaxis]
+    mask = np.arange(x.shape[1]) < y[:, None]
     return x + mask
 
 
 def deltawin(x, y):
-    idx = np.arange(y.size), y
-    w = x[idx]
-    w = w[:, np.newaxis]
-    x = x-w
-    return add_to_rows(x)
+    q = np.take_along_axis(x, y[:, None], axis=1)
+    x = x-q
+    return add_to_rows(x, y)
+
+
+def newwins(dwin, w8s, y):
+    y8s = np.take_along_axis(w8s[None, :], y[:, None, None], axis=2)
+    y0s = y8s[:, :, 0]
+    cube=w8s[None,:,:]-y0s[:,:,None]
+    dragon=dwin[:,None,:] + 2*cube
+    og = np.any(dragon>0,axis=2)
+    return og
+
+
+def blusko(k, lo, sw1, y):
+    vv = np.zeros(40)
+    vv[k] = 2
+    med = lo[2] + vv
+    hmm = med @ sw1
+    pro = np.argmax(hmm,axis=1)
+    return np.sum(pro==y)
