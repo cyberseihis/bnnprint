7b1315c44f287343227cd39553ed4554dfe75c9a
cyberseihis <mcraftdm@gmail.com>
2023-05-01 22:13:45 +0300

    Designs for bnn with centering tested

---
+++ designs/bnnparce/products/tbpendigits_bnn1_bnnparce.v
@@ -0,0 +1,47 @@
+`timescale 1us/1ns
+
+
+
+
+
+
+
+
+
+module tbpendigits_bnn1_bnnparce #(
+
+parameter FEAT_CNT = 16,
+parameter HIDDEN_CNT = 40,
+parameter FEAT_BITS = 4,
+parameter CLASS_CNT = 10,
+parameter TEST_CNT = 1000
+
+
+)();
+reg clk;
+reg [FEAT_CNT*FEAT_BITS-1:0] features;
+wire [$clog2(CLASS_CNT)-1:0] prediction;
+reg [FEAT_CNT*FEAT_BITS-1:0] testcases [0:TEST_CNT-1];
+parameter Nsperiod=5000;
+parameter period = Nsperiod/500;
+
+
+
+initial $readmemh("pendigits.memh",testcases);
+
+
+pendigits_bnn1_bnnparce dut (.features(features),.prediction(prediction));
+
+integer i;
+initial begin
+    features = testcases[0];
+    $write("[");//"
+    for(i=0;i<TEST_CNT;i=i+1) begin
+        features = testcases[i];
+        #period
+        $write("%d, ",prediction);
+    end
+    $display("]");
+end
+
+endmodule
