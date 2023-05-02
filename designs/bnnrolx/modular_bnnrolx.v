11edc5aa62d29f7752344b59605104e06053698f
cyberseihis <mcraftdm@gmail.com>
2023-05-02 20:29:13 +0300

    Rolling xnor layer in bnn seq rolling

---
+++ designs/bnnrolx/modular_bnnrolx.v
@@ -0,0 +1,33 @@
+`ifndef DUTNAME
+`define DUTNAME modular_bs
+parameter FEAT_CNT = 4;
+parameter FEAT_BITS = 4;
+parameter HIDDEN_CNT = 4;
+parameter CLASS_CNT = 4;
+`define WEIGHTS0 0
+`define WEIGHTS1 0
+`else
+    `include `BSTRINGS
+`endif
+module `DUTNAME #(
+`ifdef PARAMS
+`include `PARAMS
+`endif
+  ) (
+  input clk,
+  input rst,
+  input [FEAT_CNT*FEAT_BITS-1:0] features,
+  output [$clog2(CLASS_CNT)-1:0] prediction
+  );
+
+  localparam Weights0 = `WEIGHTS0 ;
+  localparam Weights1 = `WEIGHTS1 ;
+
+  rolx_seq #(.FEAT_CNT(FEAT_CNT),.FEAT_BITS(FEAT_BITS),.HIDDEN_CNT(HIDDEN_CNT),.CLASS_CNT(CLASS_CNT),.Weights0(Weights0),.Weights1(Weights1)) bnn (
+    .clk(clk),
+    .rst(rst),
+    .features(features),
+    .prediction(prediction)
+  );
+
+endmodule
