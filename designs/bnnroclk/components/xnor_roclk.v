f611b1f3da8c9c7bcb9526e6cbd4fce534c1f355
cyberseihis <mcraftdm@gmail.com>
2023-05-04 18:07:37 +0300

    Replaced 2nd layer to non-mem since it turned out better

--- designs/bnnroclk/components/xnor_roclk.v
+++ designs/bnnroclk/components/xnor_roclk.v
@@ -14,11 +14,23 @@ module xnor_roclk #(
   reg [$clog2(CLASS_CNT)-1:0] challenger;
   assign winner = challenger;
   localparam SUM_BITS = $clog2(HIDDEN_CNT+1);
+  wire [HIDDEN_CNT-1:0] features_n;
+
+  assign features_n = ~features;
   
-  wire [HIDDEN_CNT-1:0] slice;
-  wire [HIDDEN_CNT-1:0] xfeats;
-  assign slice = Weights[cnt*HIDDEN_CNT+:HIDDEN_CNT];
-  assign xfeats = slice ^~ features;
+  wire [HIDDEN_CNT-1:0] hybrid [CLASS_CNT-1:0];
+  genvar i,j;
+  generate
+      for(j=0;j<CLASS_CNT;j=j+1)begin
+        localparam weight = Weights[j*HIDDEN_CNT+:HIDDEN_CNT];
+        for(i=0;i<HIDDEN_CNT;i=i+1)begin
+            if(weight[i])
+                assign hybrid[j][i] = features[i];
+            else
+                assign hybrid[j][i] = features_n[i];
+        end
+      end
+  endgenerate
 
   reg [$clog2(HIDDEN_CNT+1)-1:0] soom;
   reg [$clog2(HIDDEN_CNT+1)-1:0] sofar;
@@ -26,7 +38,7 @@ module xnor_roclk #(
   always @* begin
       soom = 0;
       for (y=0;y<HIDDEN_CNT;y=y+1)
-          soom = soom + xfeats[y];
+          soom = soom + hybrid[cnt][y];
   end
   
   wire dethrone;
