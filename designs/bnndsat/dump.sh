14e60543e600cbdd2cec62ddb91c4e42b2902630
cyberseihis <mcraftdm@gmail.com>
2023-05-05 23:13:06 +0300

    Had to make dump.sh parallel cause slow. 1 sample wrong in gasid

--- designs/bnndsat/dump.sh
+++ designs/bnndsat/dump.sh
@@ -6,15 +6,15 @@ rm products/*
 rm tests/*
 for fruit in "${datasets[@]}"
 do
-    sed "s/DATASETNAME/$fruit/g" template.cmd > cmdfile.cmd
-    sed "s/TYPE/$type/g" -i cmdfile.cmd
-    sed "s/SUFFIX/$suffix/g" -i cmdfile.cmd
+    sed "s/DATASETNAME/$fruit/g" template.cmd > cmdfile${fruit}.cmd
+    sed "s/TYPE/$type/g" -i cmdfile${fruit}.cmd
+    sed "s/SUFFIX/$suffix/g" -i cmdfile${fruit}.cmd
     cp ../../trainingdata/samples/${fruit}.memh ./
-    iverilog -c cmdfile.cmd modular_${suffix}.v -o products/${fruit}_${type}_${suffix}.v -E
-    iverilog -c cmdfile.cmd tbmodular_${suffix}.v -o products/tb${fruit}_${type}_${suffix}.v -E
-    iverilog -c cmdfile.cmd products/tb${fruit}_${type}_${suffix}.v products/${fruit}_${type}_${suffix}.v -o vtmp.vvp
-    vvp  vtmp.vvp >  tests/v${fruit}_${type}_${suffix}.tst
-    rm cmdfile.cmd
-    rm vtmp.vvp
+    iverilog -c cmdfile${fruit}.cmd modular_${suffix}.v -o products/${fruit}_${type}_${suffix}.v -E
+    iverilog -c cmdfile${fruit}.cmd tbmodular_${suffix}.v -o products/tb${fruit}_${type}_${suffix}.v -E
+    iverilog -c cmdfile${fruit}.cmd products/tb${fruit}_${type}_${suffix}.v products/${fruit}_${type}_${suffix}.v -o vtmp${fruit}.vvp
+    vvp vtmp${fruit}.vvp >  tests/v${fruit}_${type}_${suffix}.tst &
+    # rm cmdfile${fruit}.cmd
+    # rm vtmp${fruit}.vvp
 done
-rm *.memh
+# rm *.memh
