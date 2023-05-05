362d222865d3d1b54cc76ce80d2bdc80643526e8
cyberseihis <mcraftdm@gmail.com>
2023-05-05 20:47:18 +0300

    Removed obsolete designs

--- designs/bnnromex/template.cmd
+++
@@ -1,9 +0,0 @@
-+define+DUTNAME=DATASETNAME_TYPE_SUFFIX
-+define+TBNAME=tbDATASETNAME_TYPE_SUFFIX
-+define+TESTCASES="DATASETNAME.memh"
-+define+PARAMS="DATASETNAME.par"
-+define+BSTRINGS="DATASETNAME_TYPE.bstr"
-+incdir+../../models/TYPE/ortho/
-+incdir+../../trainingdata/tbparams/
--y ../argmax/ 
--y components/
