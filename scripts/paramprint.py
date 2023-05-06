Fix in capped width calculation and dumped capped widths

scripts/paramprint.py
@@ -4,7 +4,27 @@ import binascii
 import re
 import os
 from hardcodew import get_bnn_filenames
-from predict import quick_bnn, max_sum1, quick_tnn
+from predict import quick_bnn, max_sum1, quick_tnn, quant
+from rounder import caps
+
+
+def capd_weights(fnm):
+    mod, X, y = quick_bnn(fnm)
+    l0 = (quant(X)*16).to_numpy()
+    wids = max_sum1(mod, X)
+    ws = mod.get_weights()
+    sw0 = np.sign(ws[0])
+    sw1 = ws[1]
+    wids = caps(l0, sw0)
+    bstr0 = matrix_bin_string(sw0.T)
+    bstr1 = matrix_bin_string(sw1.T)
+    bwids = dbytes(wids)
+    bweight = f"""\
+`define WEIGHTS0 {len(bstr0)}'b{bstr0}
+`define WEIGHTS1 {len(bstr1)}'b{bstr1}
+`define WIDTHS {8*sw0.shape[1]}'h{bwids}"""
+    with open(f"../models/bnn1/sats/{fnm}_bnn1.bstr", 'w') as file:
+        file.write(bweight)
 
 
 def ortho_weights(fnm):
@@ -37,6 +57,11 @@ def bitstr_weights(filename):
     h5.close()
     with open(f"bitstrings/{dset}.bstr", 'w') as file:
         file.write(bweight)
+
+
+def dump_sats():
+    for fnm in get_bnn_filenames():
+        capd_weights(fnm)
 
 
 def dump_orthos():