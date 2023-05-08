41198336b3fd7819d5f357583c1cc49a9f6d37cb
cyberseihis <mcraftdm@gmail.com>
2023-05-09 00:17:08 +0300

    Function to match similar neurons to similar counters tested

--- scripts/tsp.py
+++ scripts/tsp.py
@@ -8,11 +8,28 @@ from python_tsp.heuristics import solve_tsp_local_search
 def reorg(sw0, sw1):
     # assuming neuron is row and sign
     distance_matrix = dist_mat(sw0)
+    bclks = binclk(sw0.shape[0])
+    dclks = dist_mat(bclks)
     permutation, _ = solve_tsp_local_search(distance_matrix)
-    nw0 = sw0[permutation]
-    nw1 = sw1[:, permutation]
-    return nw0, nw1
+    permclks, _ = solve_tsp_local_search(dclks)
+    # Combine permutations
+    # Mostly sure but not completely
+    invclk = inv_perm(permclks)
+    trueperm = np.array(permutation)[invclk]
+    nw0 = sw0[trueperm]
+    nw1 = sw1[:, trueperm]
+    return permutation, permclks, trueperm
 
 
 def dist_mat(mat):
-    return np.sum(mat[:, np.newaxis] != mat[np.newaxis, :], axis=-1)
+    return np.array(np.sum(mat[:, np.newaxis] != mat[np.newaxis, :], axis=-1))
+
+
+def binclk(n):
+    x1 = [bin(i)[2:].zfill(6) for i in range(0, n)]
+    x2 = [list(map(int, x)) for x in x1]
+    return np.array(x2)
+
+
+def inv_perm(perm):
+    return [perm.index(i) for i in range(len(perm))]
