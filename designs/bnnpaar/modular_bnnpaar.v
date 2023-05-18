e48a39c8fc6b8dae0fc692ead7ef06e5080ea7af
cyberseihis <mcraftdm@gmail.com>
2023-05-18 18:16:48 +0300

    Starting paar design

---
+++ designs/bnnpaar/modular_bnnpaar.v
@@ -0,0 +1,83 @@
+`ifndef DUTNAME
+`define DUTNAME modular_bnnpar
+parameter FEAT_CNT = 4;
+parameter FEAT_BITS = 4;
+parameter HIDDEN_CNT = 4;
+parameter CLASS_CNT = 4;
+`define PAAR0 0
+`define YMAP 0
+`define WEIGHTS1 0
+`define WIDTHS 0
+`else
+    `include `BSTRINGS
+`endif
+module `DUTNAME #(
+`ifdef PARAMS
+`include `PARAMS
+`endif
+)(
+    input [FEAT_CNT*FEAT_BITS-1:0] features,
+    output [$clog2(CLASS_CNT)-1:0] prediction
+);
+
+localparam PAAR0 = `PAAR0 ;
+localparam YMAP = `YMAP;
+localparam ADDCNT = $bits(PAAR0) / 32;
+localparam FULLCNT = ADDCNT + (FEAT_CNT * 2);
+localparam Weights1 = `WEIGHTS1 ;
+localparam WIDTH = `WIDTHS;
+localparam SUM_BITS = $clog2(HIDDEN_CNT+1);
+localparam INDEX_BITS = $clog2(FEAT_CNT+1)+FEAT_BITS;
+wire signed [FEAT_BITS:0] feature_array [FEAT_CNT-1:0];
+wire [HIDDEN_CNT-1:0] hidden;
+wire [HIDDEN_CNT-1:0] hidden_n;
+wire [CLASS_CNT*SUM_BITS-1:0] scores; 
+assign hidden_n = ~hidden;
+
+wire signed [INDEX_BITS:0] node [FULLCNT-1:0];
+
+genvar i;
+generate
+    for(i=0;i<FEAT_CNT;i=i+1)
+        assign feature_array[i] = {1'b0,features[i*FEAT_BITS+:FEAT_BITS]};
+    for(i=0;i<FEAT_CNT;i=i+1)
+        assign node[i] = feature_array[i];
+    for(i=0;i<FEAT_CNT;i=i+1)
+        assign node[FEAT_CNT+i] = -feature_array[i];
+endgenerate
+
+integer j;
+generate
+for(i=0;i<ADDCNT;i=i+1) begin
+    localparam op1 = PAAR0[i*32+:16];
+    localparam op2 = PAAR0[i*32+16+:16];
+    localparam nodeloc = (2 * FEAT_CNT) + 1;
+    assign node[nodeloc] = node[op1] + node[op2];
+end
+for(i=0;i<HIDDEN_CNT;i=i+1) begin
+    assign hidden[i] = node[YMAP[i*16+:16]] >= 0;
+end
+endgenerate
+
+generate
+for(i=0;i<CLASS_CNT;i=i+1) begin
+    localparam Wneur = Weights1[i*HIDDEN_CNT+:HIDDEN_CNT];
+    reg unsigned [SUM_BITS-1:0] tmpscore;
+    always @ (*) begin
+        tmpscore = 0;
+        for(j=0;j<HIDDEN_CNT;j=j+1) begin
+        if(Wneur[j])
+            tmpscore = tmpscore + hidden[j];
+        else 
+            tmpscore = tmpscore + hidden_n[j];
+        end
+    end
+    assign scores[i*SUM_BITS+:SUM_BITS] = tmpscore;
+end
+endgenerate
+
+argmax #(.SIZE(CLASS_CNT),.INDEX_BITS($clog2(CLASS_CNT)),.BITS(SUM_BITS)) result (
+    .inx(scores),
+    .outimax(prediction)
+);
+endmodule
---
+++ designs/bnnpaar/products/tbcardio_bnn1_bnnpaar.v
@@ -0,0 +1,47 @@
+`timescale 1us/1ns
+
+
+
+
+
+
+
+
+
+module tbcardio_bnn1_bnnpaar #(
+
+parameter FEAT_CNT = 19,
+parameter HIDDEN_CNT = 40,
+parameter FEAT_BITS = 4,
+parameter CLASS_CNT = 3,
+parameter TEST_CNT = 1000
+
+
+)();
+reg clk;
+reg [FEAT_CNT*FEAT_BITS-1:0] features;
+wire [$clog2(CLASS_CNT)-1:0] prediction;
+reg [FEAT_CNT*FEAT_BITS-1:0] testcases [0:TEST_CNT-1];
+parameter Nsperiod=5000;
+parameter period = Nsperiod/1000;
+
+
+
+initial $readmemh("cardio.memh",testcases);
+
+
+cardio_bnn1_bnnpaar dut (.features(features),.prediction(prediction));
+
+integer i;
+initial begin
+    features = testcases[0];
+    /* $write("[");//" */
+    for(i=0;i<5;i=i+1) begin
+        features = testcases[i];
+        #period
+        $write("%d, ",prediction);
+    end
+    /* $display("]"); */
+end
+
+endmodule
